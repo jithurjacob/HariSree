50e63160d5f0c3bb06161d65012994d07deb3e78
Jithu R Jacob <jithurjacob@gmail.com>
2018-07-20 01:06:40 +0530

    base circle done

--- resources/js/KeyBoard.js
+++ resources/js/KeyBoard.js
@@ -15,8 +15,18 @@ function setKeyboardLanguage(lang){
 
 $(".key_btn").on('click',function(){
     // show an svg on top of this btn
-    
-});
+    var pos = $(this).position();
+    console.log(pos);
+
+    // $("#container").position=$(this).position();
+    $("#container").show();
+    $("#container").offset({ top: pos.top-56, left: pos.left-60 });
+
+
+})
+// $("#right_panel").on('click',function(){
+//     $("#container").hide();
+// });
 
 
 
